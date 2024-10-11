2157f58b56b87a91b349fbaa46e3690ea78221eb
dimitri-edel <dimitri.edel@gmx.net>
2024-10-11 16:34:41 +0200

    Add model class playlist to playlist app
    The Playlist class will store the items for different playlists. Each playlist is identified by the category field

--- playlist/models.py
+++ playlist/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Model class for playlist
+class Playlist(models.Model):
+    # Fields
+    '''This class represents a playlist model.'''
+    category = models.CharField(max_length=100)
+    '''category of the playlist'''
+    audio = models.FileField(upload_to='audio/', validators=[FileExtensionValidator(['mp3'])])
+    '''audio file of the item in the playlist'''
+    title = models.CharField(max_length=100)
+    '''title of the item in the playlist'''
+    description = models.TextField()
+    '''description of the item in the playlist'''
+    image = models.ImageField(upload_to='images/', validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])])
+    '''image of the item in the playlist'''
+    
+
+    # Metadata
+    class Meta:
+        verbose_name = 'Playlist'
+        verbose_name_plural = 'Playlists'
+
+    # Methods
+    def __str__(self):
+        return self.title
