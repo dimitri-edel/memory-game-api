2b77e8a42e34477b609eb7699f3023b7f5bc0c6f
dimitri-edel <dimitri.edel@gmx.net>
2024-10-12 18:44:24 +0200

    Add app named auth to settings.py and urls.py

--- memory_game_api/urls.py
+++ memory_game_api/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playlist/', include('playlist.urls')),    
+    path('auth/', include('auth.urls')),
 ]
