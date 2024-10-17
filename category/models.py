88c3bc299fbdf464908df579d01b104c472d2698
dimitri-edel <dimitri.edel@gmx.net>
2024-10-17 12:14:26 +0200

    Move model Category from playlist to category app

--- category/models.py
+++ category/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+# Model class for category
+class Category(models.Model):
+    # Fields
+    '''This class represents a category model.'''
+    name = models.CharField(max_length=100)
+    '''name of the category'''
+    description = models.TextField()
+    '''description of the category'''
+    image = models.ImageField(upload_to='images/', validators=[FileExtensionValidator(['png', 'jpg', 'jpeg', 'webp'])])
+    '''image of the category'''
+
+    # Metadata
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    # Methods
+    def __str__(self):
+        return self.name
