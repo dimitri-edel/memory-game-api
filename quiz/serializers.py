96ef190616ec7943eeb47173449bce14aa70668e
dimitri-edel <dimitri.edel@gmx.net>
2024-10-17 14:32:23 +0200

    Add Serializer for the Quiz model class

---
+++ quiz/serializers.py
@@ -0,0 +1,8 @@
+# Serializer for the Quiz model
+from rest_framework import serializers
+from .models import Quiz
+
+class QuizSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = '__all__'
