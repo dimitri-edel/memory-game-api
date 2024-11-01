1965965d2a19df86302bcbe8fbd74da369fc827e
dimitri-edel <dimitri.edel@gmx.net>
2024-11-01 17:59:12 +0100

    change the wsgi.py

--- memory_game_api/wsgi.py
+++ memory_game_api/wsgi.py
@@ -6,11 +6,20 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+sys.path.append('/opt/gameapi2/memory-game-api')
+sys.path.append('/opt/gameapi2/memory-game-api/category')
+sys.path.append('/opt/gameapi2/memory-game-api/game_admin')
+sys.path.append('/opt/gameapi2/memory-game-api/playlist')
+sys.path.append('/opt/gameapi2/memory-game-api/quiz')
+sys.path.append('/opt/gameapi2/memory-game-api/memory_game_api')
+
+
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memory_game_api.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "memory_game_api.settings")
 
 application = get_wsgi_application()
