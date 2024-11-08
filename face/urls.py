afeb5620fd18d805ceac9cd64b5fede9d8ca882c
dimitri-edel <dimitri.edel@gmx.net>
2024-11-08 16:55:45 +0100

    Add face.views.FaceUpdate
    View for updateing face datasets

--- face/urls.py
+++ face/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import FaceList
 from .views import FaceCreate
+from .views import FaceUpdate
 
 urlpatterns = [
     path('get-all/', FaceList.as_view(), name='face-list'),
     path('add/', FaceCreate.as_view(), name='face-create'),
+    path('update/<int:id>/', FaceUpdate.as_view(), name='face-update'),
 ]
--- face/views.py
+++ face/views.py
@@ -23,7 +23,7 @@ class FaceList(APIView):
 # class for creating a new face
 class FaceCreate(APIView):
     '''View for creating a new face'''
-    def post(self, request, format=None):
+    def post(self, request):
         '''Post request for creating a new face'''
         # Get token1 and token2 from the request headers
         # If the tokens are not valid, return a access denied response
@@ -36,4 +36,23 @@ class FaceCreate(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# class for updating a face
+class FaceUpdate(APIView):
+    '''View for updating a face'''
+    def put(self, request, id):
+        '''Put request for updating a face'''
+        # Get token1 and token2 from the request headers
+        # If the tokens are not valid, return a access denied response
+        token1 = request.headers.get('Token1')
+        token2 = request.headers.get('Token2')
+        user = User()
+        if not user.is_authorized(token1, token2):
+            return Response(status=status.HTTP_401_UNAUTHORIZED)        
+        face = Face.objects.get(id=id)
+        serializer = FaceSerializer(face, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
