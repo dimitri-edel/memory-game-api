265c973101ffc569659e0ee948e9414a8302b61e
dimitri-edel <dimitri.edel@gmx.net>
2024-11-08 17:16:36 +0100

    Add style.views.StyleList
    View for listing all the styles

--- style/urls.py
+++ style/urls.py
@@ -1,1 +1,6 @@
 from django.urls import path
+from .views import StyleList
+
+urlpatterns = [
+    path('get-all/', StyleList.as_view(), name='style-list'),
+]
--- style/views.py
+++ style/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Style
+from .serializers import StyleSerializer
+from game_admin.authentication import User
+from memory_game_api.settings import ALLOWED_CLIENT_HOSTS
 
-# Create your views here.
+# View for listing all styles
+class StyleList(APIView):
+    '''View for listing all styles'''
+    def get(self, request, format=None):
+        '''Get request for listing all styles'''
+        if(request.META['HTTP_ORIGIN'] not in ALLOWED_CLIENT_HOSTS):
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        styles = Style.objects.all()
+        serializer = StyleSerializer(styles, many=True)
+        return Response(serializer.data)
+
