fe1cffa354fc248ec4e718dffb77b57da46aea36
dimitri-edel <dimitri.edel@gmx.net>
2024-11-08 18:04:52 +0100

    Add face.views.FaceListByCategory
    View for fetching a list of face images based on their category_id

--- face/urls.py
+++ face/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import FaceList
+from .views import FaceListByCategory
 from .views import FaceCreate
 from .views import FaceUpdate
 from .views import FaceDelete
 
 urlpatterns = [
     path('get-all/', FaceList.as_view(), name='face-list'),
+    path('get-by-category/<int:category_id>/', FaceListByCategory.as_view(), name='face-list-by-category'),
     path('add/', FaceCreate.as_view(), name='face-create'),
     path('update/<int:id>/', FaceUpdate.as_view(), name='face-update'),
     path('delete/<int:id>/', FaceDelete.as_view(), name='face-delete'),
--- face/views.py
+++ face/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from face.models import Face
+from category.models import Category
 from face.serializers import FaceSerializer
 from game_admin.authentication import User
 from memory_game_api.settings import ALLOWED_CLIENT_HOSTS
@@ -15,6 +16,18 @@ class FaceList(APIView):
         faces = Face.objects.all()
         serializer = FaceSerializer(faces, many=True)
         return Response(serializer.data)
+    
+# class for getting a list of faces for a category
+class FaceListByCategory(APIView):
+    '''View for listing all faces for a category'''
+    def get(self, request, category_id):
+        '''Get request for listing all faces for a category'''
+        if(request.META['HTTP_ORIGIN'] not in ALLOWED_CLIENT_HOSTS):
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        faces = Face.objects.filter(category=category_id)
+        serializer = FaceSerializer(faces, many=True)
+        return Response(serializer.data)
+    
 
 # class for creating a new face
 class FaceCreate(APIView):
