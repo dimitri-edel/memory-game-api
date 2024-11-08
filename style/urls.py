870ff2517af08645f98e31973fbf84c39f218a71
dimitri-edel <dimitri.edel@gmx.net>
2024-11-08 17:18:03 +0100

    Add style.views.StyleCreate
    View for ctrateing a new style

--- style/urls.py
+++ style/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import StyleList
+from .views import StyleCreate
 
 urlpatterns = [
     path('get-all/', StyleList.as_view(), name='style-list'),
+    path('add/', StyleCreate.as_view(), name='style-create'),
 ]
--- style/views.py
+++ style/views.py
@@ -17,3 +17,20 @@ class StyleList(APIView):
         serializer = StyleSerializer(styles, many=True)
         return Response(serializer.data)
 
+# class for creating a new style
+class StyleCreate(APIView):
+    '''View for creating a new style'''
+    def post(self, request):
+        '''Post request for creating a new style'''
+        # Get token1 and token2 from the request headers
+        # If the tokens are not valid, return a access denied response
+        token1 = request.headers.get('Token1')
+        token2 = request.headers.get('Token2')
+        user = User()
+        if not user.is_authorized(token1, token2):
+            return Response(status=status.HTTP_401_UNAUTHORIZED)        
+        serializer = StyleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
