cbddfb4a25eff271e80f178eb779cc8256588297
dimitri-edel <dimitri.edel@gmx.net>
2024-11-09 17:27:34 +0100

    add print statement for debugging in style.views.StyleUpdate

--- style/views.py
+++ style/views.py
@@ -72,6 +72,8 @@ class StyleUpdate(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        else:
+            print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # class for deleting a style
